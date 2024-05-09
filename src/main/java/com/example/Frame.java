95db181b0b64a2ffe03a2ab93f97af05a374491f
BlueBomber49 <alj88@byu.edu>
2024-05-09 13:26:42 -0600

    Added original code back for Frame and Main

---
+++ src/main/java/com/example/Frame.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import javax.swing.JFrame;
+
+public class Frame extends JFrame{
+    
+
+    public Frame(){
+
+        this.setTitle("Weather Rock");
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setSize(500, 400);
+        this.setVisible(true);
+    }
+}
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,20 @@
 package com.example;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public static void main(String[] args) throws Exception {
+        Frame mainFrame = new Frame();
+
+        JPanel panel = new JPanel();
+
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
+        panel.setBackground(Color.blue);
+
+        Button button = new Button("button");
+        panel.add(button);
+        mainFrame.add(panel, BorderLayout.CENTER);
+        
     }
 }
